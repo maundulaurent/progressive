99d206d16b8474187c4a17ce6a90729b8e771a55
CHARLES MAUNDU <charlesmaundu16@gmail.com>
2024-08-09 22:05:52 +0300

    adding the pages

--- evaluation.php
+++ evaluation.php
@@ -54,77 +54,15 @@ $conn->close();
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   </head>
   <body>
-    <div id="preloader-active">
-      <div class="preloader d-flex align-items-center justify-content-center">
-        <div class="preloader-inner position-relative">
-          <div class="page-loading">
-            <div class="page-loading-inner">
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <header class="header sticky-bar">
-      <div class="container">
-        <div class="main-header">
-          <div class="header-left">
-            <div class="header-logo"><a class="d-flex" href="index.php"><h3 class="text-white">Recipe</h3></a></div>
-            <div class="header-nav">
-              <nav class="nav-main-menu d-none d-xl-block">
-                <ul class="main-menu">
-                  <li ><a class="active" href="index.php">Home</a></li>
-                  <li ><a href="about.php">About</a></li>
-                  <li><a href="contact.php">Contact</a></li>
-                </ul>
-              </nav>
-              <div class="burger-icon burger-icon-white"><span class="burger-icon-mid"></span><span class="burger-icon-bottom"></span></div>
-            </div>
-            <div class="header-right">
-              <div class="d-none d-xxl-inline-block align-middle mr-10"><a class="text-14-medium call-phone color-white hover-up" href="">+254 23 056 447</a></div>
-              <div class="d-none d-xxl-inline-block box-dropdown-cart align-middle mr-10"><span class="text-14-medium icon-list icon-account"><span class="text-14-medium color-white arrow-down">EN</span></span>
-                <div class="dropdown-account">
-                  <ul>
-                    <li><a class="font-md" href="#"><img src="assets/imgs/template/icons/en.png" alt="luxride">
-                        English</a></li>
-                    <li><a class="font-md" href="#"><img src="assets/imgs/template/icons/fr.png" alt="luxride">
-                        French</a></li>
-                    <li><a class="font-md" href="#"><img src="assets/imgs/template/icons/cn.png" alt="luxride">
-                        Chiness</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="box-button-login d-inline-block mr-10 align-middle"><a class="btn btn-default hover-up" href="dashboard.php">Recipes</a></div>
-              <!-- <div class="box-button-login d-none2 d-inline-block align-middle"><a class="btn btn-white hover-up" href="login.php">Admin</a></div> -->
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-    <div class="mobile-header-active mobile-header-wrapper-style perfect-scrollbar">
-      <div class="mobile-header-wrapper-inner">
-        <div class="mobile-header-content-area">
-          <div class="perfect-scroll">
-            <div class="mobile-menu-wrap mobile-header-border">
-              <nav class="mt-15">
-                <ul class="mobile-menu font-heading">
-                <li ><a class="active" href="index.php">Home</a></li>
-                  <li ><a href="about.php">About</a></li>
-                  <li><a href="contact.php">Contact</a></li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+
+  <?php include_once 'includes/preloader.php'; ?>
+
+  <?php include_once 'includes/navbar.php'; ?>
 
 
-    <main class="main">
+  <main class="main">
     <section class="section">
-
+      
     <div class="container mt-4">
     <div class="box-booking-tabs"> 
             <div class="item-tab wow fadeInUp"><a href="dashboard.php">
@@ -153,6 +91,14 @@ $conn->close();
         <h3 class="mb-4 mt-70">Recipe Evaluation for <?php echo htmlspecialchars($recipe['name']); ?></h3>
         <div class="box-row-tab mt-50">
         <div class="box-tab-left">
+        <div class="box-frature-image mb-60 wow fadeInUp"> 
+        <div class="cardImage"> 
+          <div class="datePost"> 
+            <div class="heading-52-medium color-white">14.</div>
+            <p class="text-14 color-white">Jun, 2022</p>
+          </div><img src="assets/imgs/page/blog2/img-single.png" alt="luxride">
+        </div>
+        </div>
         <h4 class="mb-8">Ingredients for this recipe:</h4>
           <div class="row mt-10">
             <div class="col-md-8">
@@ -311,7 +257,7 @@ $conn->close();
     </div>
     </section>
 
-   </main>
+  </main>
 
     <script>
     $(document).ready(function() {
@@ -469,73 +415,9 @@ $conn->close();
     </script>
 
 
-    <footer class="footer">
-      <div class="footer-1">
-        <div class="container-sub">
-          <div class="box-footer-top">
-            <div class="row align-items-center">
-              <div class="col-lg-6 col-md-6 text-md-start text-center mb-15 wow fadeInUp">
-                <div class="d-flex align-items-center justify-content-md-start justify-content-center"><a class="mr-30" href="#"><h3 class="text-light">Recipe Generator</h3></a><a class="text-14-medium call-phone color-white hover-up" href="tel:+254723056447">+254723056447</a></div>
-              </div>
-              <div class="col-lg-6 col-md-6 text-md-end text-center mb-15 wow fadeInUp">
-                <div class="d-flex align-items-center justify-content-md-end justify-content-center"><span class="text-18-medium color-white mr-10">Follow Us</span><a class="icon-socials icon-facebook" href="#"></a><a class="icon-socials icon-twitter" href="#"></a><a class="icon-socials icon-instagram" href="#"></a><a class="icon-socials icon-linkedin" href="#"></a></div>
-              </div>
-            </div>
-          </div>
-          <div class="row mb-40">
-            <div class="col-lg-4 width-20">
-              <h5 class="text-18-medium color-white mb-20 wow fadeInUp">Our Offices</h5>
-            </div> 
-            <div class="col-lg-4 width-30">
-              <h5 class="text-18-medium color-white mb-20 wow fadeInUp">3rd Godown after Odds and Ends, Next to Plaza 2000, Mombasa Road</h5>
-            </div> 
-            <div class="col-lg-4 width-30">
-              <h3 class="text-18-medium color-white mb-20 wow fadeInUp">About Us</h3>
-              <h5 class="text-18-medium color-white mb-20 wow fadeInUp">Are you an aspiring baker in Kenya today?  Bakewave offers end-to-end solutions in the baking industry in Kenya.</h5>
-            </div>          
-          </div>
-        </div>
-      </div>
-      <div class="footer-2">
-        <div class="container-sub">
-          <div class="footer-bottom">
-            <div class="row align-items-center">
-              <div class="col-lg-8 col-md-12 text-center text-lg-start"><span class="text-14 color-white mr-50">© 2024 Recipe Generator</span>
-                <ul class="menu-bottom">
-                  <li><a href="">Terms</a></li>
-                  <li><a href="">Privacy policy</a></li>
-                  <li><a href="">Legal notice</a></li>
-                  <li><a href="#">Accessibility</a></li>
-                </ul>
-              </div>
-              <div class="col-lg-4 col-md-12 text-center text-lg-end"><a class="btn btn-link-location" href="#">Nairobi</a><a class="btn btn-link-globe active" href="#">Kenya</a></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <?php include_once 'includes/footer.php'; ?>
 
-    <script src="assets/js/vendors/modernizr-3.6.0.min.js"></script>
-    <script src="assets/js/vendors/jquery-3.6.0.min.js"></script>
-    <script src="assets/js/vendors/jquery-migrate-3.3.0.min.js"></script>
-    <script src="assets/js/vendors/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/vendors/waypoints.js"></script>
-    <script src="assets/js/vendors/wow.js"></script>
-    <script src="assets/js/vendors/magnific-popup.js"></script>
-    <script src="assets/js/vendors/perfect-scrollbar.min.js"></script>
-    <script src="assets/js/vendors/select2.min.js"></script>
-    <script src="assets/js/vendors/isotope.js"></script>
-    <script src="assets/js/vendors/scrollup.js"></script>
-    <script src="assets/js/vendors/swiper-bundle.min.js"></script>
-    <script src="assets/js/vendors/noUISlider.js"></script>
-    <script src="assets/js/vendors/slider.js"></script>
-    <!-- Count down-->
-    <script src="assets/js/vendors/counterup.js"></script>
-    <script src="assets/js/vendors/jquery.countdown.min.js"></script>
-    <!-- Count down--><script src="assets/js/vendors/jquery.elevatezoom.js"></script>
-<script src="assets/js/vendors/slick.js"></script>
-<script src="assets/js/vendors/jquery-ui.js"></script>
-<script src="assets/js/vendors/jquery.timepicker.min.js"></script>
-    <script src="assets/js/main.js?v=1.0.0"></script>
+    <?php include_once 'includes/scripts.php'; ?>
+
   </body>
 </html>
